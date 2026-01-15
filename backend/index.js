54ad919c8a317916f1d0dedc674cb3afa73e141a
NewennT <newenn.turbiau@gmail.com>
2026-01-15 16:45:50 +0100

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+console.log("Node backend starting...");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
