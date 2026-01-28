15220059e7b15666acf6aa3e8d5a5ba1b75d2918
NewennT <newenn.turbiau@gmail.com>
2026-01-28 13:45:11 +0100

    Header & footer

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -3,17 +3,35 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class CoreModule { 
