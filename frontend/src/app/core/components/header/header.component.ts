b6832243eed9bf70e5f9b80ddfec13cea9da28a4
NewennT <newenn.turbiau@gmail.com>
2026-01-30 10:48:15 +0100

    Logout depuis le header

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,5 +13,12 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent {
   faHouse = faHouse;
   faBars = faBars;
+
+  constructor(public auth: AuthService, private router: Router) { }
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/']);
+  }
+
 
 }
