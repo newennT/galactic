3118355686f3798b63be91deb8fc7fee92c22131
NewennT <newenn.turbiau@gmail.com>
2026-02-03 14:25:20 +0100

    Affichage exercice type pairs

--- frontend/src/app/chapters/components/chapter-detail/chapter-detail.component.ts
+++ frontend/src/app/chapters/components/chapter-detail/chapter-detail.component.ts
@@ -4,6 +4,7 @@ import { map, Observable } from 'rxjs';
 import { Chapter } from 'src/app/core/models/chapter.model';
 import { Page } from 'src/app/core/models/page.model';
 import { UniqueResponses } from 'src/app/core/models/uniqueResponses.model';
+import { Pairs } from 'src/app/core/models/pairs.model';
 
 @Component({
   selector: 'app-chapter-detail',
@@ -53,6 +54,8 @@ export class ChapterDetailComponent implements OnInit{
   showFeedback: { [pageId: number ]: boolean } = {};
   isCorrect: { [pageId: number ]: boolean } = {};
 
+
+  // ---------- Question à réponse unique 
   validateUnique(page: Page){
     if(!page.Exercise || page.Exercise.type !== "UNIQUE") return;
 
@@ -65,7 +68,53 @@ export class ChapterDetailComponent implements OnInit{
     );
 
     this.isCorrect[pageId] = selectedResponse?.is_correct ?? false;
-    
+  }
+
+  // ---------- Question key-pairs
+  currentSelection: Pairs[] = [];
+  matchedIds = new Set<number>();
+  wrongIds = new Set<number>();
+
+  pairsCache: { [pageId: number]: Pairs[] } = {};
+
+  getShuffledPairs(page: Page): Pairs[] {
+    const pageId = page.id_page;
+    if (!this.pairsCache[pageId]) {
+      this.pairsCache[pageId] = page.Exercise?.Pairs?.sort(() => Math.random() - 0.5) ?? [];
+    }
+    return this.pairsCache[pageId];
+  }
+
+  selectPairs(item: Pairs, page: Page){
+    if(this.matchedIds.has(item.id_response)) return;
+
+    this.currentSelection.push(item);
+    if(this.currentSelection.length < 2) return;
+    const [a, b] = this.currentSelection;
+
+    if(a.pair_key === b.pair_key){
+      this.matchedIds.add(a.id_response);
+      this.matchedIds.add(b.id_response);
+      this.checkPairsCompleted(page);
+    } else {
+      this.wrongIds.add(a.id_response);
+      this.wrongIds.add(b.id_response);
+
+      setTimeout(() => {
+        this.wrongIds.delete(a.id_response);
+        this.wrongIds.delete(b.id_response);
+      }, 800);
+    }
+
+    this.currentSelection = [];
+  }
+
+  checkPairsCompleted(page: Page){
+    const total = page.Exercise?.Pairs?.length ?? 0;
+
+    if(this.matchedIds.size === total){
+      this.showFeedback[page.id_page] = true;
+    }
   }
   
 
